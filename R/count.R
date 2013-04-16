013b53dc1f3536d9277a737f8299f988f6ce8f32
Peter DeWitt <peter.dewitt@ucdenver.edu>
2013-04-16 13:07:42 -0600

    count.R has big.mark added for the n

--- R/count.R
+++ R/count.R
@@ -3,6 +3,7 @@ function(var, val, data, equal.or.in = "equal",
                   show.n = TRUE,
                   show.percent = TRUE,
                   digits = getOption("qwraps.percent.digits", 1),
+                  big.mark = ",",
                   latex  = getOption("qwraps.latex", TRUE))
 {
   if (equal.or.in == "equal")
@@ -15,5 +16,5 @@ function(var, val, data, equal.or.in = "equal",
     n <- sum(data[, var] %in% val) 
     p <- n / dim(data)[1]
   }
-  paste(n, " (", frmt(p*100, digits), "\\%) ", sep = "")
+  paste(frmt(n, digits = 0, big.mark = big.mark),  " (", frmt(p*100, digits), "\\%) ", sep = "")
 }
