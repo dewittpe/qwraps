0d78d65667414160790b21de69fd80b325147f68
Peter DeWitt <peter.dewitt@ucdenver.edu>
2013-04-25 11:50:34 -0600

    fixed error in params.coxph when only one predictor was in the model

--- R/params.coxph.R
+++ R/params.coxph.R
@@ -31,7 +31,7 @@
 params.coxph <-
 function(fit, alpha = getOption("qwraps.alpha", 0.05), ...)
 {
-  rtn <- summary(fit)$coef[, -2]
+  rtn <- matrix(summary(fit)$coef[, -2], ncol = 4)
   rtn[, 3] <- rtn[, 1] + qnorm(1 - alpha / 2) * rtn[, 2]
   rtn[, 2] <- rtn[, 1] + qnorm(alpha / 2) * rtn[, 2]
 
